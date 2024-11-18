65992e0a436c59a88713c9a2c9da7af88c157dea
Nicolas Mendonca <nicolasmendonca@gmail.com>
2024-11-18 11:16:53 -0300

    initial commit

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,6 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+vim.opt.relativenumber = true
+vim.opt.foldtext = 'v:lua.vim.treesitter.foldtext()'
+vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,14 +12,41 @@ return {
       require "configs.lspconfig"
     end,
   },
-
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "smoka7/hop.nvim",
+    config = function()
+      local hop = require('hop')
+      hop.setup({
+        keys = 'htnsueoaidgcrlypmbkjvx',
+        multi_windows = true,
+        uppercase_labels = true,
+      })
+      
+      -- Set up keymapping for web-like navigation
+      vim.keymap.set({'n', 'x', 'o'}, '<leader>fj', function()
+        hop.hint_words()
+      end, {silent = true, desc = 'Hop to word'})
+    end,
+    keys = {
+      {"<leader>fj", mode = {"n", "x", "o"}, desc = "Hop to word"}
+    },
+  },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css", "typescript", "javascript", "tsx", "svelte"
+  		},
+  		highlight = {
+  			enable = true,
+  		},
+  		indent = {
+  			enable = true,
+  		},
+  		fold = {
+  			enable = true
+  		},
+  	},
+  }
 }
